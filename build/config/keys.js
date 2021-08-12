c009139da9b30a89b103ae8aac46b632e0407bb7
coracer0 <77650683+coracer0@users.noreply.github.com>
2021-08-12 16:52:34 -0500

    configuracion de base de datos
    Configuracion para produccion

--- build/config/keys.js
+++ build/config/keys.js
@@ -2,10 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
     database: {
-        host: 'localhost',
-        port: 3306,
+        host: 'db-mysql-nyc3-86925-do-user-9679766-0.b.db.ondigitalocean.com',
+        port: 25060,
         user: 'admin',
-        password: 'admin',
+        password: 'ddwx1x00wex3aj8d',
         database: 'db_category'
     }
 };
